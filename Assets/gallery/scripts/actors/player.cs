e8b615d245b53a5172199f8f12f8825a2ed54013
Steven Rivera <ghostszmusic@gmail.com>
2017-10-29 19:14:48 -0400

    init
    init

---
+++ Assets/gallery/scripts/actors/player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(statsManager))]
+public class player : MonoBehaviour {
+
+    public Image img;
+    public Camera cam;
+    float speed = 35.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+#if UNITY_ANDROID || UNITY_IOS
+
+        mobileControls();
+
+#else
+
+        controllerControls();
+
+#endif
+
+
+
+    }
+
+    void mobileControls() {
+
+        Vector3 dir = Vector3.zero;
+        dir.x = -Input.acceleration.y;
+        dir.y = Input.acceleration.x;
+        if (dir.sqrMagnitude > 1){
+
+            dir.Normalize();
+        }
+
+        dir *= Time.deltaTime;
+        transform.Translate(dir * speed);
+
+    }
+
+    void controllerControls() {
+
+        Vector3 dir = Vector3.zero;
+
+        dir.x = -Input.GetAxis("Mouse Y");
+        dir.y = 0;
+        dir.z = 0;
+
+        dir *= Time.deltaTime;
+        cam.transform.Rotate(dir * speed);
+
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/gallery/scripts/managers/statsManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class stats {
+
+    public string character { get; set; }
+    public int lvl { get; set; }
+    public int hp { get; set; }
+    public int atk { get; set; }
+    public int def { get; set; }
+    public int magic { get; set; }
+    public int exp { get; set; }
+    public int expNeeded { get; set; }
+
+}
+
+public class equipmentList {
+
+    public GameObject model { get; set; }
+    public string name { get; set; }
+    public string type { get; set; }
+    public string desc { get; set; }
+    public int lvl { get; set; }
+    public int hp { get; set; }
+    public int mp { get; set; }
+    public int atk { get; set; }
+    public int def { get; set; }
+    public int magic { get; set; }
+
+}
+
+public class statsManager : MonoBehaviour {
+
+    public List<equipmentList> el = new List<equipmentList>(0);
+    public string actorsPath;
+    public string expPath;
+    public string itemsPath;
+    public string savePath;
+    public string readme;
+
+    void Start(){
+
+        actorsPath = Application.dataPath + "/scripts/stats/actors.json";
+        expPath = Application.dataPath + "/scripts/stats/exp.json";
+        itemsPath = Application.dataPath + "/scripts/stats/items.json";
+        savePath = Application.persistentDataPath + "/GhostszMusic/Shooting Gallery/data/Save.sav";
+        readme = Application.persistentDataPath + "/GhostszMusic/Shooting Gallery/custom/readme.txt";
+
+#if UNITY_EDITOR
+        Debug.Log(fileCheck(actorsPath));
+        Debug.Log(fileCheck(expPath));
+        Debug.Log(fileCheck(itemsPath));
+        Debug.Log(fileCheck(savePath));
+        Debug.Log(fileCheck(readme));
+#endif
+
+    }
+
+	public static int damageTaken(stats attacker, stats defender) {
+
+        int damage = 0;
+
+        damage = attacker.atk - defender.def;
+
+        return damage;
+    }
+
+    public static int getExp(string character) {
+
+        return 0;
+    }
+
+    public void saveNewItemToList(equipmentList e) {
+
+
+
+    }
+
+    public string fileCheck(string f) {
+        string msg = "";
+
+        if (File.Exists(f)) {
+
+            msg = f + " was found";
+
+        } else {
+
+            msg = "Error: " + f + " was not found!";
+        }
+
+        return msg;
+    }
+
+}
